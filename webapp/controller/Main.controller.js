d35b213529f35d32c8922abda4528562f52c801a
Lea Gozar <lea.adelle.gozar@accenture.com>
2025-08-12 06:48:59 +0000

    Additional comments in controller

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -8,13 +8,22 @@ sap.ui.define([
     return Controller.extend("sapips.training.odata.controller.Main", {
         onInit() {
             var oTable = this.byId("tabProducts");
-            
+
             var oFilter = new Filter({
                 path: "Discontinued",
                 operator: FilterOperator.EQ,
                 value1: true
             })
             
+            // direct does not work::
+            // oBinding = oTable.getBinding("items"); 
+            // oBinding.filter(oFilter) 
+
+            //reference: UI5 Demo kit -> OData V4 Tutorial -> Step 4
+            //var oView = this.getView();
+            //oView.byId("tabProducts").getBinding("items").filter(oFilter);
+
+            //reference: UI5 Demo kit -> Get Started -> Worklist App Tutorial -> Step 4
             oTable.attachEventOnce("updateFinished", function () {
                 const oBinding = oTable.getBinding("items");
                 if (oBinding) {
